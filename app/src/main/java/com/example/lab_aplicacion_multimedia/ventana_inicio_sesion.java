db922303dd8f4626461ac40ee65a366d18d44a16
Carlos <72311521+NoHackNoFun@users.noreply.github.com>
2021-10-21 11:11:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab_aplicacion_multimedia/ventana_inicio_sesion.java
@@ -0,0 +1,30 @@
+package com.example.lab_aplicacion_multimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ventana_inicio_sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_inicio_sesion);
+    }
+
+    /**
+     *
+     * Descripcion: Oyente asociado al boton Registro que inicializa la actividad para Registrarse
+     *
+     * @param view
+     *
+     *Guada esto es una prueba
+     */
+    public void oyente_inciar_registro(View view){
+        Intent actividad_registro = new Intent(this, ventana_registro.class );
+        startActivity(actividad_registro);
+    }
+
+}
---
+++ app/src/main/java/com/example/lab_aplicacion_multimedia/ventana_registro.java
@@ -0,0 +1,14 @@
+package com.example.lab_aplicacion_multimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventana_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_registro);
+    }
+}
